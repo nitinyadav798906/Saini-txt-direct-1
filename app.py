472af16b44ce24ba73732989faa996be3c335522
nitinyadav798906 <nitinyadav798906@gmail.com>
2025-04-14 23:47:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,9 +10,9 @@ def hello_world():
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="stylesheet" type="text/css" href="https://tinypic.host/images/2025/02/07/DeWatermark.ai_1738952933236-1.png">
+	<link rel="stylesheet" type="text/css" href="https://tinypic.host/images/2025/04/14/1000000208.th.jpg">
     <titleSudoR2spr Repository</title>
-	<link rel="icon" type="image/x-icon" href="https://tinypic.host/images/2025/02/07/DeWatermark.ai_1738952933236-1.png">
+	<link rel="icon" type="image/x-icon" href="https://tinypic.host/images/2025/04/14/1000000208.th.jpg">
 
 </head>
 
@@ -33,9 +33,9 @@ def hello_world():
     </div>
 	<br></br><br></br><br></br>
 	<footer class="bg-dark text-white text-center py-3 mt-5">
-	<center><img loading="lazy" class="object-none object-center" src="https://tinypic.host/images/2025/02/07/DeWatermark.ai_1738952933236-1.png" width="240" height="120">
+	<center><img loading="lazy" class="object-none object-center" src="https://tinypic.host/images/2025/04/14/1000000208.th.jpg" width="240" height="120">
         Powered By SAINI 
-		<img loading="lazy" class="object-none object-center" src="https://tinypic.host/images/2025/02/07/DeWatermark.ai_1738952933236-1.png" width="240" height="120">
+		<img loading="lazy" class="object-none object-center" src="https://tinypic.host/images/2025/04/14/1000000208.th.jpg" width="240" height="120">
 		<div class="footer__copyright">
             <p class="footer__copyright-info">
                 © 2024 Video Downloader. All rights reserved.
